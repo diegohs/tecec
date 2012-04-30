f34f00bd6eabfd91cb56dc8bb715393565d640b1
Bruno <Bruno@Bruno-PC>
2012-04-30 03:03:47 -0300

    Código Tecec sem a UI classe Student

--- tecec.dto/src/tecec/dto/Course.java
+++ tecec.dto/src/tecec/dto/Course.java
@@ -2,20 +2,20 @@ package tecec.dto;
 
 public class Course {
 	private String pKCourse;
-	private String name;	
-	
+	private String name;
+
 	public String getPKCourse() {
 		return pKCourse;
 	}
-	
+
 	public void setPKCourse(String pKCourse) {
 		this.pKCourse = pKCourse;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
--- tecec.ui.contract/src/tecec/ui/contract/NewCourseController.java
+++ tecec.ui.contract/src/tecec/ui/contract/NewCourseController.java
@@ -2,7 +2,8 @@ package tecec.ui.contract;
 
 public interface NewCourseController {
 	tecec.dto.Course getCourse();
-	
+
 	void storeCourse();
+
 	String getInvalidFieldsMessage();
 }
