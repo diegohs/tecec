f9a7a2d294693f868298e7ff0a0110e312abf920
bruno <bruno@bruno-desktop>
2012-06-12 14:33:07 -0300

    Backup feito, nao consegui fazer o restore, implemente.
    Bruno

--- tecec.ui/src/tecec/ui/MainUI.java
+++ tecec.ui/src/tecec/ui/MainUI.java
@@ -1,20 +1,59 @@
 package tecec.ui;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JMenu;
+import javax.swing.JOptionPane;
 
 import tecec.ui.contract.control.IMainUIController;
 import tecec.ui.contract.view.IMainUI;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 public class MainUI implements IMainUI {
 
-	private JFrame frame;
+	private JFrame frame;	
 
+	private void backupImmediate() {
+
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setDialogTitle("Efetuar backup da base de dados");
+
+		int variableChooser = fileChooser.showSaveDialog(null);
+
+		if (variableChooser != JFileChooser.APPROVE_OPTION) {
+			JOptionPane.showMessageDialog(null, "Backup não realizado", "Janela finalizada", JOptionPane.CANCEL_OPTION);
+		} else {
+
+			String commandDump = "";
+
+			if (System.getProperty("os.name").toUpperCase().compareTo("LINUX") == 0) {
+				commandDump = "/usr/bin/mysqldump";
+			} else {				
+				commandDump = "MYSQL_PATH\\bin\\mysqldump.exe";
+			}
+
+			String location = fileChooser.getSelectedFile() + ".sql";
+			ProcessBuilder pb = new ProcessBuilder(commandDump, "--user=root",
+					"--password=lester", "tecec", "--result-file=" + location);
+			try {
+				pb.start();
+				JOptionPane.showMessageDialog(null, "Backup efetuado com sucesso");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				JOptionPane.showMessageDialog(null, e.toString(), "Erro", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}
+	
+	private void restoreBackup () {
+		// implementar nao consegui: Bruno
+	}
+	
 	@Override
 	public void setVisible(boolean visible) {
 		frame.setVisible(visible);
@@ -28,48 +67,45 @@ public class MainUI implements IMainUI {
 		initialize();
 	}
 
-	private void showStudentUI(){
+	private void showStudentUI() {
 		mainUIController.showStudentViewerUI();
 	}
 
-	private void showCourseUI(){
+	private void showCourseUI() {
 		mainUIController.showCouseViewerUI();
 	}
 
-	private void showAreaUI(){
+	private void showAreaUI() {
 		mainUIController.showAreaViewerUI();
 	}
 
-	private void showActivityUI(){
+	private void showActivityUI() {
 		mainUIController.showActivityViewerUI();
 	}
 
-	private void showAdvisorUI(){
+	private void showAdvisorUI() {
 		mainUIController.showAdvisorViewerUI();
 	}
 
-	private void showStatusUI(){
+	private void showStatusUI() {
 		mainUIController.showStatusViewerUI();
 	}
-	
-	private void showMonographUI(){
+
+	private void showMonographUI() {
 		mainUIController.showMonographViewerUI();
 	}
-	
-	private void showProfileUI () {
+
+	private void showProfileUI() {
 		mainUIController.showProfileViewerUI();
 	}
-	
-	
-	
-	private void showStageUI () {
+
+	private void showStageUI() {
 		mainUIController.showStageViewerUI();
 	}
-	
-	private void showPermissionUI () {
+
+	private void showPermissionUI() {
 		mainUIController.showPermissionViewerUI();
 	}
-	
 
 	private void initialize() {
 		JFrame.setDefaultLookAndFeelDecorated(true);
@@ -122,7 +158,7 @@ public class MainUI implements IMainUI {
 			}
 		});
 		mnCadastro.add(mntmOrientador);
-		
+
 		JMenuItem mntmMonografia = new JMenuItem("Monografia");
 		mntmMonografia.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -138,7 +174,7 @@ public class MainUI implements IMainUI {
 			}
 		});
 		mnCadastro.add(mntmStatus);
-		
+
 		JMenuItem mntmProfile = new JMenuItem("Perfil");
 		mntmProfile.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -146,19 +182,18 @@ public class MainUI implements IMainUI {
 			}
 		});
 		mnCadastro.add(mntmProfile);
-		
-		
+
 		JMenuItem mntmStage = new JMenuItem("Estágio");
-		mntmStage.addActionListener(new ActionListener () {
+		mntmStage.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				showStageUI ();
+				showStageUI();
 			}
 		});
-		
+
 		mnCadastro.add(mntmStage);
-		
+
 		JMenuItem mntmPermission = new JMenuItem("Permissão");
 		mntmPermission.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -166,6 +201,32 @@ public class MainUI implements IMainUI {
 			}
 		});
 		mnCadastro.add(mntmPermission);
+
+		JMenu mnBackup = new JMenu("Backup");
+		menuBar.add(mnBackup);
+
+		/* Efetua o backup no local desejado */
+
+		JMenuItem mntmEfetuar = new JMenuItem("Efetuar");
+		mntmEfetuar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				/* Backup aqui */
+				backupImmediate();
+			}
+		});
+		mnBackup.add(mntmEfetuar);
+
+		
+		JMenuItem mntmRestaurar = new JMenuItem("Restaurar");
+		mntmRestaurar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				/* Restaurar aqui */
+				restoreBackup();
+			}
+		});
+		
+		mnBackup.add(mntmRestaurar);
+
 	}
 
 }
